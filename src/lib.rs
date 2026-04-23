4bc7d806bbcb277f9eb21c943765e1f124e29f3f
Chris <chrisroberts1080@gmail.com>
2026-04-23 16:10:11 +0100

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,7 @@ pub use geometry::Rect;
 pub use layout::Constraint;
 pub use style::{Color, Span, Style};
 pub use terminal::{Frame, Terminal, TerminalHandle};
-pub use widget::{Block, Bordered, BoxedWidget, HStack, Spinner, SpinnerStyle, Text, VStack, Widget, WidgetExt, boxed};
+pub use widget::{
+    Block, Bordered, BoxedWidget, HStack, Spinner, SpinnerStyle, Text, VStack, Widget, WidgetExt,
+    boxed,
+};
